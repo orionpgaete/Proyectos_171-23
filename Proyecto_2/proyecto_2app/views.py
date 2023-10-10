577e5f730c0cb2e827e2af702381791dd12ff235
orionpgaete <pedro.gaete@pgcomputer.cl>
2023-10-10 13:13:59 -0300

    proyecto 3- DB

--- Proyecto_2/proyecto_2app/views.py
+++ Proyecto_2/proyecto_2app/views.py
@@ -6,8 +6,13 @@ def index (request):
     return render(request, 'web\index.html')
 
 def electronica(request):
-    data = { "titulo": "Electronica"
-
+    data = { "titulo": "Electronica",
+            "producto1": "Mouse Inalambrico",
+            "producto2": "Luz Led",
+            "producto3": "Tester Digital",
+            "foto1": "electronica1.jpg",
+            "foto2": "electronica2.jpg",
+            "foto3": "electronica3.jpg"
     }
     return render(request, 'web/producto.html', data)
 
@@ -18,7 +23,13 @@ def ropa(request):
     return render(request, 'web/producto.html', data)
 
 def juguete(request):
-    data = {"titulo": "Juguete"
+    data = {"titulo": "Juguete",
+            "producto1": "Juego Didactico",
+            "producto2": "Autos",
+            "producto3": "Muñeca Grande",
+            "foto1": "juguete1.jpg",
+            "foto2": "juguete2.jpg",
+            "foto3": "juguete3.jpg"
 
     }
     return render(request, 'web/producto.html', data)
